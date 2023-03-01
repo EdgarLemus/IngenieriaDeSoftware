7b8b3b2637eb468edbe4d635336d3ee541001d9b
EdgarLemusSQA <edgar.lemus@sqasa.co>
2023-03-01 15:19:10 -0500

    pruebas unitarias

--- src/test/java/com/gildedrosesolucion/GildedRoseTest.java
+++ src/test/java/com/gildedrosesolucion/GildedRoseTest.java
@@ -1,8 +1,8 @@
 package com.gildedrosesolucion;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.gildedrose.Item;
@@ -10,36 +10,104 @@ import com.gildedrose.solucion.GildedRose;
 
 public class GildedRoseTest {
 
-	@Test
-    void testUpdateQuality() {
-        Item[] items = new Item[] { new Item("foo", 0, 0), 
-                                    new Item("Aged Brie", 10, 5), 
-                                    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                                    new Item("Sulfuras, Hand of Ragnaros", 0, 80)};
-        
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        
-        // Comprobar que los valores de calidad se actualizan correctamente al cabo de un día
+	private Item[] items;
+    private GildedRose gildedRose;
+
+    @BeforeEach
+    void setUp() {
+        items = new Item[] { new Item("foo", 1, 5) };
+        gildedRose = new GildedRose(items);
+    }
+
+    @Test
+    void testUpdateQualitySellInPositive() {
+        gildedRose.updateQuality();
+        assertEquals(4, items[0].sellIn);
+        assertEquals(9, items[0].quality);
+    }
+
+    @Test
+    void testUpdateQualitySellInNegative() {
+        items[0].sellIn = -1;
+        gildedRose.updateQuality();
+        assertEquals(-2, items[0].sellIn);
+        assertEquals(8, items[0].quality);
+    }
+
+    @Test
+    void testUpdateQualityQualityZero() {
+        items[0].quality = 0;
+        gildedRose.updateQuality();
         assertEquals(0, items[0].quality);
-        assertEquals(6, items[1].quality);
-        assertEquals(21, items[2].quality);
-        assertEquals(80, items[3].quality);
-        
-        // Compruebe que los valores de sellIn se actualizan correctamente al cabo de un día
+    }
+
+    @Test
+    void testUpdateQualityQualityFifty() {
+        items[0].quality = 50;
+        gildedRose.updateQuality();
+        assertEquals(49, items[0].quality);
+    }
+
+    @Test
+    void testUpdateQualityAgedBrie() {
+        items[0] = new Item("Aged Brie", 2, 0);
+        gildedRose.updateQuality();
+        assertEquals(1, items[0].sellIn);
+        assertEquals(1, items[0].quality);
+    }
+
+    @Test
+    void testUpdateQualityBackstagePasses() {
+        items[0] = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 10);
+        gildedRose.updateQuality();
+        assertEquals(10, items[0].sellIn);
+        assertEquals(11, items[0].quality);
+
+        items[0] = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10);
+        gildedRose.updateQuality();
+        assertEquals(9, items[0].sellIn);
+        assertEquals(12, items[0].quality);
+
+        items[0] = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10);
+        gildedRose.updateQuality();
+        assertEquals(4, items[0].sellIn);
+        assertEquals(13, items[0].quality);
+
+        items[0] = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10);
+        gildedRose.updateQuality();
         assertEquals(-1, items[0].sellIn);
-        assertEquals(9, items[1].sellIn);
-        assertEquals(14, items[2].sellIn);
-        assertEquals(0, items[3].sellIn);
-        
-        // Comprobar que los valores de calidad nunca son negativos
-        assertTrue(items[0].quality >= 0);
-        assertTrue(items[1].quality >= 0);
-        assertTrue(items[2].quality >= 0);
-        assertTrue(items[3].quality >= 0);
-        
-        // Comprueba que los valores de calidad nunca sean superiores al 50
-        assertTrue(items[1].quality <= 50);
-        assertTrue(items[2].quality <= 50);
+        assertEquals(0, items[0].quality);
+    }
+
+    @Test
+    void testUpdateQualitySulfuras() {
+        items[0] = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
+        gildedRose.updateQuality();
+        assertEquals(-1, items[0].sellIn);
+        assertEquals(78, items[0].quality);
+    }
+    
+    @BeforeEach
+    void beforeEach() {
+        items = new Item[] { new Item("New Item 1", 5, 10),
+                             new Item("New Item 2", 0, 5),
+                             new Item("New Item 3", 5, 50),
+                             new Item("New Item 4", -1, 30) };
+        gildedRose = new GildedRose(items);
+    }
+
+    @Test
+    void testUpdateQualityForNewCategory() {
+    	gildedRose.updateQualityForNewCategory(items);
+
+        assertEquals(10, items[0].quality);
+        assertEquals(5, items[1].quality);
+        assertEquals(50, items[2].quality);
+        assertEquals(30, items[3].quality);
+
+        assertEquals(5, items[0].sellIn);
+        assertEquals(0, items[1].sellIn);
+        assertEquals(5, items[2].sellIn);
+        assertEquals(-1, items[3].sellIn);
     }
 }
